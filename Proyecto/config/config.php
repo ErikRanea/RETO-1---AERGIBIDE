d0b7ac3b2577af79ee20c14b80e38fc1214a4614
Erik Ranea <erik.ranea@ikasle.egibide.org>
2024-10-21 12:49:01 +0200

    Estructura de carpetas creadas

---
+++ Proyecto/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOST", "p-mysql.arriaga.eu");
+define("DB", "grupo6_2425");
+define("DB_USER", "grupo6_2425");
+define("DB_PASS", "F(t[Hj-rC5XRKtGj");
+
+define("DEFAULT_CONTROLLER", "Tema");
+define("DEFAULT_ACTION", "List");
---
+++ Proyecto/view/layout/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <header class="mb-5">
+        <div class="p-5 text-center bg" style="margin-top:
+58px;">
+            <h1 class="mb-3"><?php echo $controller->page_title; ?>
+            </h1>
+            <h4 class="mb-3">Este proyecto muestra cómo hacer una aplicación web MVC con PHP.</h4>
+            <h4 class="mb-3">El objetivo es mostrar cómo realizar una aplicación utilizando éste patrón de diseño   </h4>
+        </div>
+    </header>
