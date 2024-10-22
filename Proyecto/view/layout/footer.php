ebe516c03aca9d78e1f30eb7c953caa457987273
jason <jasonalejandro.varas@ikasle.egibide.org>
2024-10-22 11:34:49 +0200

    Header y Footer

--- Proyecto/view/layout/footer.php
+++ Proyecto/view/layout/footer.php
@@ -1,4 +1,14 @@
 </div>
+<!-- Comienzo Footer -->
+<div class="footer">
+    <span>AERGIBIDE</span>
+    <div class="redes iconos">
+        <i class="bi bi-instagram"></i>
+        <i class="bi bi-twitter-x"></i>
+        <i class="bi bi-facebook"></i>
+    </div>
+</div>
+<!-- Fin Footer -->
 </body>
 <script src="assets/js/bootstrap.min.js" ></script>
 </html>
