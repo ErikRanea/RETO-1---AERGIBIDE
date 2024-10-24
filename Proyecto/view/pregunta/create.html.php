1a2f6f51124e0d514d4f5adea190b7af12b7ae5e
jason <jasonalejandro.varas@ikasle.egibide.org>
2024-10-24 13:06:54 +0200

    Crear Pregunta (sin estilo)

---
+++ Proyecto/view/pregunta/create.html.php
@@ -0,0 +1,73 @@
+<?php
+// DATOS USUARIO SESION INICIADA
+$usuario = $_SESSION["user_data"];
+
+if (isset($_GET["id_tema"])) $id_tema = $_GET["id_tema"];
+if (isset($dataToView["temas"])) $temas = $dataToView["temas"];
+
+date_default_timezone_set('Europe/Madrid'); // Establece la zona horaria
+$fecha_hora = new DateTime();
+$fecha_hora = $fecha_hora->format("Y-m-d H:i:s");
+print_r($fecha_hora)
+
+?>
+
+<div class="row">
+    <h1>Crear Pregunta</h1>
+    <a href="javascript:window.history.back()">Volver</a>
+    <?php
+    if(isset($_GET["response"]) and $_GET["response"] === true): ?>
+        <div class="alert alert-success">
+            Operación realizada correctamente. <a href="index.php?controller=pregunta&action=list">Volver al listado</a>
+        </div>
+    <?php endif; ?>
+
+    <form class="form" action="index.php?controller=pregunta&action=save" method="POST" enctype="multipart/form-data">
+        <input type="hidden" name="id_usuario" value="<?= $usuario["id"] ?>">
+
+
+        <?php if (!isset($_GET["id_tema"])): ?>
+            <div>
+                <label>
+                    Tema
+                    <select id="" name="id_tema">
+                        <?php foreach ($temas as $tema):?>
+                            <option value="<?= $tema["id"] ?>"><?= $tema["nombre"] ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </label>
+            </div>
+            <br>
+        <?php else: ?>
+            <input type="hidden" name="id_tema" value="<?= $id_tema; ?>" />
+        <?php endif; ?>
+        <div>
+            <label>
+                Titulo
+                <br>
+                <input type="text" name="titulo" placeholder="Titulo de tu Pregunta">
+            </label>
+        </div>
+        <br>
+
+        <div>
+            <label>
+                Describe tu problema
+                <br>
+                <textarea name="texto" placeholder="Descripcio de tu Problema"></textarea>
+            </label>
+        </div>
+        <br>
+
+        <div>
+            <label>
+                Adjuntar archivo
+                <input type="file" name="imagen">
+            </label>
+        </div>
+        <br>
+
+        <input type="submit" value="Guardar" class="btn btn-primary"/>
+    </form>
+
+</div>
--- Proyecto/view/pregunta/list.html.php
+++ Proyecto/view/pregunta/list.html.php
@@ -1,9 +1,9 @@
 
 <?php
 if (isset($dataToView["tema"])) $tema = $dataToView["tema"];
-if (isset($dataToView["preguntas"])) $preguntas = $dataToView["preguntas"];
+if (isset($dataToView["pregunta"])) $preguntas = $dataToView["pregunta"];
 if (isset($dataToView["paginas"])) $paginas = $dataToView["paginas"];
-//if (isset($dataToView["preguntas"])) $usuario = $preguntas["usuario"] ;
+//if (isset($dataToView["pregunta"])) $usuario = $pregunta["usuario"] ;
 //if (isset());
 ?>
 
@@ -11,16 +11,16 @@ if (isset($dataToView["paginas"])) $paginas = $dataToView["paginas"];
 <div>
     <h1><?php echo $tema["nombre"] ?></h1>
     <button>
-        <a href="index.php?controller=preguntas&action=create">Crear Pregunta</a>
+        <a href="index.php?controller=pregunta&action=create&id_tema=<?= $tema["id"] ?>">Crear Pregunta</a>
     </button>
     <?php if (count($preguntas) > 0): ?>
         <?php foreach ($preguntas as $pregunta): ?>
-        <?php $usuario = $pregunta["usuario"] ?>
+        <?php $usuario = $pregunta["usuario"]; ?>
             <div class="pregunta">
                 <p>
                     <?= $pregunta["titulo"] ?>
                 </p>
-                <div>
+                <div class="datos-pregunta">
                     <span><?= $pregunta["votos"] ?> Votos</span>
 
                     <div>
@@ -37,7 +37,7 @@ if (isset($dataToView["paginas"])) $paginas = $dataToView["paginas"];
                 <!-- Enlace a la página anterior -->
                 <?php if ($paginas[0] > 1): ?>
                     <li class="page-item">
-                        <a class="page-link" href="index.php?controller=preguntas&action=list&id_tema=<?= $tema["id"] ?>&page=<?= $paginas[0] - 1; ?>">Anterior</a>
+                        <a class="page-link" href="index.php?controller=pregunta&action=list&id_tema=<?= $tema["id"] ?>&page=<?= $paginas[0] - 1; ?>">Anterior</a>
                     </li>
                 <?php else: ?>
                     <li class="page-item disabled">
@@ -48,14 +48,14 @@ if (isset($dataToView["paginas"])) $paginas = $dataToView["paginas"];
                 <!-- Enlaces de número de página -->
                 <?php for ($i = 1; $i <= $paginas[1]; $i++): ?>
                     <li class="page-item <?= ($i == $paginas[0]) ? 'active' : ''; ?>">
-                        <a class="page-link" href="index.php?controller=preguntas&action=list&id_tema=<?= $tema["id"] ?>&page=<?= $i; ?>"><?= $i; ?></a>
+                        <a class="page-link" href="index.php?controller=pregunta&action=list&id_tema=<?= $tema["id"] ?>&page=<?= $i; ?>"><?= $i; ?></a>
                     </li>
                 <?php endfor; ?>
 
                 <!-- Enlace a la página siguiente -->
                 <?php if ($paginas[0] < $paginas[1]): ?>
                     <li class="page-item">
-                        <a class="page-link" href="index.php?controller=preguntas&action=list&id_tema=<?= $tema["id"] ?>&page=<?= $paginas[0] + 1; ?>">Siguiente</a>
+                        <a class="page-link" href="index.php?controller=pregunta&action=list&id_tema=<?= $tema["id"] ?>&page=<?= $paginas[0] + 1; ?>">Siguiente</a>
                     </li>
                 <?php else: ?>
                     <li class="page-item disabled">
