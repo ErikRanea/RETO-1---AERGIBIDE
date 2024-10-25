511574895a22161605a4a347f89a71292f97318e
Erik Ranea <erik.ranea@ikasle.egibide.org>
2024-10-25 10:37:03 +0200

    Pregunta hecho

--- Proyecto/view/pregunta/view.html.php
+++ Proyecto/view/pregunta/view.html.php
@@ -1,24 +1,21 @@
-<div class="contenedorPreguntaYRespuesta">
-    <div class="cont1">
+<div class="contenedorPreguntasYRespuestas">
+    <div class="contenedorPregunta">
         <div class="fotoUsuarioPregunta">
-
-            <img src="<?php
-           // $fotoPerfilDefecto = $_COOKIE["darkMode"] == "enabled" ? "assets/img/fotoPorDefectoBlack.png" : "assets/img/fotoPorDefecto.png" ;
-             echo isset($dataToView["usuario"]) ? $dataToView["usuario"]["foto_perfil"] : "assets/img/fotoPorDefecto.png" ?>" alt="">
+            <img src="assets/img/fotoPorDefecto.png" alt="Foto de usuario">
+        </div>
+        <div class="preguntaTitulo">
+            Título de la Pregunta
+        </div>
+        <div class="descripcionPregunta">
+            Descripción de la pregunta aquí.
+        </div>
+        <div class="panelDeBotones">
+            Botones de acción
         </div>
     </div>
 
-
-    <div class="cont2">
-        <div class="divPregunta">
-            <h1><strong><?php echo isset($dataToView["pregunta"]) ? $dataToView["pregunta"]["titulo"] : "TituloNoEncontrado"?></strong></h1>
-            <p><?php echo isset($dataToView["pregunta"]) ? $dataToView["pregunta"]["texto"] : "Texto no encontrado"?></p>
-            <?php if($dataToView["pregunta"]["imagen"] != null)?><img src="<?php echo $dataToView["pregunta"]["imagen"]?>" alt=""><?php;?>
-        </div>
+    <div class="contenedorRespuestaDivididor">
+        
     </div>
 
-
-    <div class="cont3">
-        %20
-    </div>
 </div>
