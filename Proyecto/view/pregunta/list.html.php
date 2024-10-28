560ed11e52988d4bc48f5b64a7117904b3c7a49e
jason <jasonalejandro.varas@ikasle.egibide.org>
2024-10-28 09:36:02 +0100

    Pregunta Estilo (lista, crear)

--- Proyecto/view/pregunta/list.html.php
+++ Proyecto/view/pregunta/list.html.php
@@ -7,65 +7,43 @@ if (isset($dataToView["paginas"])) $paginas = $dataToView["paginas"];
 //if (isset());
 ?>
 
+<div class="contenedorListaPreguntas">
 
-<div>
-    <h1><?php echo $tema["nombre"] ?></h1>
-    <button>
-        <a href="index.php?controller=pregunta&action=create&id_tema=<?= $tema["id"] ?>">Crear Pregunta</a>
-    </button>
-    <?php if (count($preguntas) > 0): ?>
-        <?php foreach ($preguntas as $pregunta): ?>
-        <?php $usuario = $pregunta["usuario"]; ?>
-            <div class="pregunta">
-                <p>
-                    <?= $pregunta["titulo"] ?>
-                </p>
-                <div class="datos-pregunta">
-                    <span><?= $pregunta["votos"] ?> Votos</span>
+    <div class="listaPreguntas">
+        <div class="encabezado">
+            <h1><?php echo $tema["nombre"] ?></h1>
+            <a href="index.php?controller=pregunta&action=create&id_tema=<?= $tema["id"] ?>" class="btn btnCrear">Crear Pregunta</a>
+        </div>
 
-                    <div>
-                        <?= $usuario["username"] ?>
-                        <?= $pregunta["fecha_hora"] ?>
+        <?php if (count($preguntas) > 0): ?>
+            <?php foreach ($preguntas as $pregunta): ?>
+            <?php $usuario = $pregunta["usuario"]; ?>
+                <div class="pregunta">
+                    <p>
+                        <a href="index.php?controller=respuesta&action=view&id_pregunta=<?= $pregunta["id"] ?>" class="tituloPregunta"><?= $pregunta["titulo"] ?></a>
+                    </p>
+                    <div class="datos-pregunta">
+                        <span><?= $pregunta["votos"] ?> Votos</span>
+
+                        <div>
+                            <?= $usuario["username"] ?>
+                            <?= $pregunta["fecha_hora"] ?>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <hr>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
 
-        <nav aria-label="Paginación de preguntas" class="">
-            <ul class="pagination justify-content-center">
-                <!-- Enlace a la página anterior -->
-                <?php if ($paginas[0] > 1): ?>
-                    <li class="page-item">
-                        <a class="page-link" href="index.php?controller=pregunta&action=list&id_tema=<?= $tema["id"] ?>&page=<?= $paginas[0] - 1; ?>">Anterior</a>
-                    </li>
-                <?php else: ?>
-                    <li class="page-item disabled">
-                        <span class="page-link">Anterior</span>
-                    </li>
-                <?php endif; ?>
-
+            <div class="pagination">
                 <!-- Enlaces de número de página -->
                 <?php for ($i = 1; $i <= $paginas[1]; $i++): ?>
-                    <li class="page-item <?= ($i == $paginas[0]) ? 'active' : ''; ?>">
-                        <a class="page-link" href="index.php?controller=pregunta&action=list&id_tema=<?= $tema["id"] ?>&page=<?= $i; ?>"><?= $i; ?></a>
-                    </li>
+                    <a class="page-btn <?= ($i == $paginas[0]) ? 'active' : ''; ?>" href="index.php?controller=pregunta&action=list&id_tema=<?= $tema["id"] ?>&page=<?= $i; ?>">
+                        <?= $i; ?>
+                    </a>
                 <?php endfor; ?>
+            </div>
 
-                <!-- Enlace a la página siguiente -->
-                <?php if ($paginas[0] < $paginas[1]): ?>
-                    <li class="page-item">
-                        <a class="page-link" href="index.php?controller=pregunta&action=list&id_tema=<?= $tema["id"] ?>&page=<?= $paginas[0] + 1; ?>">Siguiente</a>
-                    </li>
-                <?php else: ?>
-                    <li class="page-item disabled">
-                        <span class="page-link">Siguiente</span>
-                    </li>
-                <?php endif; ?>
-            </ul>
-        </nav>
-
-    <?php else: ?>
-        <p>No hay preguntas sobre ese tema</p>
-    <?php endif;?>
+        <?php else: ?>
+            <p>No hay preguntas sobre ese tema</p>
+        <?php endif;?>
+    </div>
 </div>
