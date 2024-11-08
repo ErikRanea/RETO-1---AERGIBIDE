4faa25fc62d0b39ea278592497147b097e4d4130
jason <jasonalejandro.varas@ikasle.egibide.org>
2024-11-08 12:29:42 +0100

    Configuracion Usuario

--- Proyecto/view/usuario/vistaPrincipal/actividad.html.php
+++ Proyecto/view/usuario/vistaPrincipal/actividad.html.php
@@ -1,13 +1,12 @@
 <?php
 $usuario = $_SESSION["user_data"];
-
 ?>
 
 <div class="panelActividad">
     <div class="panelBotones">
-        <button class="btn btnCrear btnPanel" id="btnPregunta">Preguntas</button>
-        <button class="btn btnCrear btnPanel" id="btnRespuesta">Respuestas</button>
-        <button class="btn btnCrear btnPanel" id="btnGuardado">Guardados</button>
+        <button class="btn btnCrear btnPanelActividad" id="btnPregunta">Preguntas</button>
+        <button class="btn btnCrear btnPanelActividad" id="btnRespuesta">Respuestas</button>
+        <button class="btn btnCrear btnPanelActividad" id="btnGuardado">Guardados</button>
 
         <input type="hidden" value="<?= $usuario["username"] ?>" id="username">
     </div>
---
+++ Proyecto/view/usuario/vistaPrincipal/gestionUsuario.html.php
@@ -0,0 +1,12 @@
+
+<div class="panelGestion">
+    <div class="botonesArriba">
+        <button class="btn btnCrear btnPanelGestion" id="btnGestion">Gestión Usuarios</button>
+        <button class="btn btnCrear btnPanelGestion" id="btnNuevousuario">Nuevo Usuario</button>
+    </div>
+
+    <div class="contenidoGestion" id="contenidoGestion">
+
+    </div>
+</div>
+
