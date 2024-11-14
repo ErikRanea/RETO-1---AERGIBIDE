b2eb1074ea0c937a1173ef0618f4d212418996fb
IbaiSaenzDeBuruaga <ibai.saenzdeburuaga@ikasle.egibide.org>
2024-11-14 11:57:45 +0100

    requiere added

--- Proyecto/view/usuario/gestion/nuevoUsuario.html.php
+++ Proyecto/view/usuario/gestion/nuevoUsuario.html.php
@@ -21,31 +21,31 @@
             <form action="index.php?controller=usuario&action=create" method="post" id="userDataForm" class="user-data-form">
                 <div class="form-field">
                     <label for="nombre" class="field-label">Nombre:</label>
-                    <input type="text" name="nombre" class="field-input" value="">
+                    <input type="text" name="nombre" class="field-input" value="" required>
                 </div>
                 <div class="form-field">
                     <label for="apellido" class="field-label">Apellido:</label>
-                    <input type="text" name="apellido" class="field-input" value="">
+                    <input type="text" name="apellido" class="field-input" value="" required>
                 </div>
                 <div class="form-field">
                     <label for="username" class="field-label">Usuario:</label>
-                    <input type="text" name="username" class="field-input" value="">
+                    <input type="text" name="username" class="field-input" value="" required>
                 </div>
                 <div class="form-field">
                     <label for="email" class="field-label">Email:</label>
-                    <input type="text" name="email" class="field-input" value="">
+                    <input type="text" name="email" class="field-input" value="" required>
                 </div>
                 <div class="form-field">
                     <label for="password" class="field-label">Contraseña:</label>
-                    <input type="password" name="password" class="field-input" value="">
+                    <input type="password" name="password" class="field-input" value="" required>
                 </div>
                 <div class="form-field">
                     <label for="repetirPassword" class="field-label">Repetir contraseña:</label>
-                    <input type="password" name="repetirPassword" id="actualPassword" class="field-input" value="">
+                    <input type="password" name="repetirPassword" id="actualPassword" class="field-input" value="" required>
                 </div>
                 <div class="form-field">
                     <label for="rol" class="field-label">Rol:</label>
-                    <select name="rol" id="rol" class="field-input">
+                    <select name="rol" id="rol" class="field-input" required>
                         <option value="user">Usuario</option>
                         <?php if ($_SESSION["user_data"]["rol"] == "gestor"): ?>
                             <option value="admin">Administrador</option>
